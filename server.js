4d6f6cdb4f4b650f28d35badabf205856d106dda
Simon <therealslimv@yahoo.de>
2020-01-09 17:08:40 +0100

    fixed a wrong path

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static(__dirname + '/dist/calendar-web'));
 
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname+'/dist/calendar-app/index.html'))
+    res.sendFile(path.join(__dirname+'/dist/calendar-web/index.html'))
 });
 
 app.listen(process.env.PORT || 8080);
